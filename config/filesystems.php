c9e997090a2207d7ab46bb07118112b24685524b
ahmed <ahmed@convident.nl>
2022-03-23 17:55:11 +0200

    linking storage 0.2

--- config/filesystems.php
+++ config/filesystems.php
@@ -67,7 +67,7 @@ return [
 
     'links' => [
         // public_path('storage') => storage_path('app/public'),
-        '/home4/vestate1/public_html/storage' => storage_path('app/public'),
+        '/home4/vestate1/public_html/crm/storage' => storage_path('app/public'),
     ],
 
 ];
